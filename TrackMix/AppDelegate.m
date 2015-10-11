3d62e590ade1191b0bce91db89f73e9448e6f0ca
JessRudder <thejessicarudder@gmail.com>
2015-10-11 18:42:51 -0400

    Add logging to actions

--- TrackMix/AppDelegate.m
+++ TrackMix/AppDelegate.m
@@ -24,8 +24,18 @@
 }
 
 - (IBAction)mute:(id)sender {
+    NSLog(@"received a mute: message");
 }
 
 - (IBAction)takeFloatValueForVolumeFrom:(id)sender {
+    NSString *senderName = nil;
+    
+    if (sender == self.textField) {
+        senderName = @"textField";
+    }
+    else {
+        senderName = @"slider";
+    }
+    NSLog(@"%@ sent takeFloatValueForVolumeFrom: with value %1.2f", senderName, [sender floatValue]);
 }
 @end
