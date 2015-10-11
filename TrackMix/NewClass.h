30832c974d02527d7114dada19ddb1c0dc782674
JessRudder <thejessicarudder@gmail.com>
2015-10-11 19:45:54 -0400

    Add min window size constraints

---
+++ TrackMix/NewClass.h
@@ -0,0 +1,13 @@
+//
+//  NewClass.h
+//  
+//
+//  Created by Jessica Rudder on 10/11/15.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NewClass : NSObject
+
+@end
---
+++ TrackMix/NewClass.m
@@ -0,0 +1,13 @@
+//
+//  NewClass.m
+//  
+//
+//  Created by Jessica Rudder on 10/11/15.
+//
+//
+
+#import "NewClass.h"
+
+@implementation NewClass
+
+@end
